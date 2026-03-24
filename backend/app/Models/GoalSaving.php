47a1f1e500d25ba5282a7872fac3ec2948d0ba69
andratyg <indraandra545@gmail.com>
2026-03-24 22:14:19 +0700

    menambahkan models dan migrations goals dan goals saving

---
+++ backend/app/Models/GoalSaving.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class GoalSaving extends Model
+{
+    protected $fillable = ['goal_id', 'amount', 'note', 'date'];
+
+    public function goal() {
+        return $this->belongsTo(Goal::class);
+    }
+}
